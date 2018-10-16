dabe53764a222ee95bf4dacf748fdab95ab00347
zhi <zzqlrr@gmail.com>
2018-10-16 22:04:51 +0800

    sync

--- src/main/scala/com/zhi/TextWordCount.scala
+++ src/main/scala/com/zhi/TextWordCount.scala
@@ -7,18 +7,41 @@ import scala.util.control.NonFatal
 
 object TextWordCount {
     def main(args: Array[String]): Unit = {
+        /**
+          * val params: ParameterTool = ParameterTool.fromArgs(args)
+          *
+          * // set up execution environment
+          * val env = ExecutionEnvironment.getExecutionEnvironment
+          *
+          * // make parameters available in the web interface
+          *     env.getConfig.setGlobalJobParameters(params)
+          * val text =
+          * if (params.has("input")) {
+          *         env.readTextFile(params.get("input"))
+          * } else {
+          * println("Executing WordCount example with default input data set.")
+          * println("Use --input to specify file input.")
+          *         env.fromCollection(WordCountData.WORDS)
+          * }
+          */
         val inpath = try {
             ParameterTool.fromArgs(args).get("inpath")
-        }catch {
-            case NonFatal(e)=>
+        } catch {
+            case NonFatal(e) =>
                 System.err.println("No file specified. Please run 'TextWordCount --inpath <path>'")
                 return
         }
 
-        val env=ExecutionEnvironment.getExecutionEnvironment
-        val text=env.readTextFile(inpath)
-        val res=text.flatMap{_.split(" ") filter{_.nonEmpty}}.map{(_,1)}.groupBy(0).sum(1)
-        res.writeAsCsv("./res.txt","\n"," ")
+        val env = ExecutionEnvironment.getExecutionEnvironment
+        val text = env.readTextFile(inpath)
+        val res = text.flatMap {
+            _.split(" ") filter {
+                _.nonEmpty
+            }
+        }.map {
+            (_, 1)
+        }.groupBy(0).sum(1)
+        res.writeAsCsv("./res.txt", "\n", " ")
         env.execute("textwordcount")
     }
 }
