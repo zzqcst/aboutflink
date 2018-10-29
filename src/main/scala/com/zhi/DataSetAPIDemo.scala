b6002b66270f1f8cd9a907e40a8902364277eaba
zhi <zzqlrr@gmail.com>
2018-10-29 22:34:21 +0800

    sync

--- src/main/scala/com/zhi/DataSetAPIDemo.scala
+++ src/main/scala/com/zhi/DataSetAPIDemo.scala
@@ -1,5 +1,10 @@
 package com.zhi
 
-object DataSetAPIDemo {
+import org.apache.flink.api.scala.ExecutionEnvironment
 
+object DataSetAPIDemo {
+    def main(args: Array[String]): Unit = {
+        val env = ExecutionEnvironment.getExecutionEnvironment
+        val text = env.fromElements("hello","there")
+    }
 }
